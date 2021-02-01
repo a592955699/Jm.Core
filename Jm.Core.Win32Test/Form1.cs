5562c84b22bd4b3f662a24524223f97d3a627353
hujunmin <rober.hu@hotmail.com>
2021-02-01 23:47:29 +0800

    新增  win32api 的代码调整

---
+++ Jm.Core.Win32Test/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Jm.Core.WinForm.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jm.Core.Win32Test
+{
+    public partial class Form1 : Form
+    {
+        GlobalHooks _GlobalHooks;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _GlobalHooks = new GlobalHooks(this.Handle);
+            _GlobalHooks.Shell.WindowCreated += new GlobalHooks.WindowEventHandler(WindowCreated);
+            _GlobalHooks.Shell.Start();
+        }
+
+        private void WindowCreated(IntPtr Handle)
+        {
+            int capacity = CommonApis.GetWindowTextLength(new HandleRef(null, Handle)) * 2;
+            StringBuilder stringBuilder = new StringBuilder(capacity);
+            CommonApis.GetWindowText(new HandleRef(null, Handle), stringBuilder, stringBuilder.Capacity);
+            Console.WriteLine(stringBuilder.ToString());
+
+            int id = 0;
+            CommonApis.GetWindowThreadProcessId(Handle, out id);
+            if (id != 0)
+            {
+                System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(id);
+                Console.WriteLine(p.MainModule.FileName);
+
+                ////Loop through and see if this is a program we want to monitor
+                //for (int i = 0; i < settings.Count; i++)
+                //{
+                //    ProgramSettings set = settings[i];
+                //    if ((p.MainModule.FileName.ToLower() == set.Filepath.ToLower()) && (set.Enabled))
+                //    {
+                //        ModifyProperties(set, Handle);
+
+                //        //Show a balloon tip if we're set to
+                //        if (checkBalloonStart.Checked)
+                //        {
+                //            trayIcon.ShowBalloonTip(3000, "Program Profile Applied", "Window Manager has applied a profile to " + p.MainModule.ModuleName, ToolTipIcon.Info);
+                //        }
+                //    }
+                //}
+            }
+        }
+    }
+}
---
+++ Jm.Core/WinForm/Win32/CommonApis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jm.Core.WinForm.Win32
+{
+    public class CommonApis
+    {
+        #region User32
+        [DllImport("User32", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(HandleRef hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+
+        //Enumerate top-level windows
+        [DllImport("User32")]
+        public static extern int EnumWindows(EnumWindowsCallback callback, int extraData);
+        
+        public delegate bool EnumWindowsCallback(int hWnd, int lParam);
+
+        //Get Process ID from Handle
+        [DllImport("user32.dll")]
+        public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int ID);
+        #endregion
+    }
+}
