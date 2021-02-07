b9a7ddf71868cbbd7873bc340064a462496e700f
hujunmin <rober.hu@hotmail.com>
2021-02-07 14:56:36 +0800

    新增传奇地图寻路

---
+++ Jm.Core.MIr2Test/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Jm.Core.MIr2Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_mapinfo = new System.Windows.Forms.GroupBox();
+            this.lbl_end = new System.Windows.Forms.Label();
+            this.lbl_start = new System.Windows.Forms.Label();
+            this.lbl_point = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_findPath = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_map = new System.Windows.Forms.Panel();
+            this.pic_map = new System.Windows.Forms.PictureBox();
+            this.lbl_info = new System.Windows.Forms.Label();
+            this.btn_brower = new System.Windows.Forms.Button();
+            this.gb_mapinfo.SuspendLayout();
+            this.panel_map.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gb_mapinfo
+            // 
+            this.gb_mapinfo.Controls.Add(this.btn_brower);
+            this.gb_mapinfo.Controls.Add(this.lbl_info);
+            this.gb_mapinfo.Controls.Add(this.lbl_end);
+            this.gb_mapinfo.Controls.Add(this.lbl_start);
+            this.gb_mapinfo.Controls.Add(this.lbl_point);
+            this.gb_mapinfo.Controls.Add(this.label9);
+            this.gb_mapinfo.Controls.Add(this.label8);
+            this.gb_mapinfo.Controls.Add(this.label7);
+            this.gb_mapinfo.Controls.Add(this.btn_findPath);
+            this.gb_mapinfo.Controls.Add(this.label6);
+            this.gb_mapinfo.Controls.Add(this.label5);
+            this.gb_mapinfo.Controls.Add(this.label4);
+            this.gb_mapinfo.Controls.Add(this.label3);
+            this.gb_mapinfo.Controls.Add(this.label2);
+            this.gb_mapinfo.Controls.Add(this.label1);
+            this.gb_mapinfo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gb_mapinfo.Location = new System.Drawing.Point(0, 0);
+            this.gb_mapinfo.Name = "gb_mapinfo";
+            this.gb_mapinfo.Size = new System.Drawing.Size(170, 461);
+            this.gb_mapinfo.TabIndex = 0;
+            this.gb_mapinfo.TabStop = false;
+            this.gb_mapinfo.Text = "Map info";
+            // 
+            // lbl_end
+            // 
+            this.lbl_end.AutoSize = true;
+            this.lbl_end.Location = new System.Drawing.Point(70, 132);
+            this.lbl_end.Name = "lbl_end";
+            this.lbl_end.Size = new System.Drawing.Size(17, 12);
+            this.lbl_end.TabIndex = 12;
+            this.lbl_end.Text = "--";
+            // 
+            // lbl_start
+            // 
+            this.lbl_start.AutoSize = true;
+            this.lbl_start.Location = new System.Drawing.Point(70, 109);
+            this.lbl_start.Name = "lbl_start";
+            this.lbl_start.Size = new System.Drawing.Size(17, 12);
+            this.lbl_start.TabIndex = 11;
+            this.lbl_start.Text = "--";
+            // 
+            // lbl_point
+            // 
+            this.lbl_point.AutoSize = true;
+            this.lbl_point.Location = new System.Drawing.Point(70, 87);
+            this.lbl_point.Name = "lbl_point";
+            this.lbl_point.Size = new System.Drawing.Size(17, 12);
+            this.lbl_point.TabIndex = 10;
+            this.lbl_point.Text = "--";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(70, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 12);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "--";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "--";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "--";
+            // 
+            // btn_findPath
+            // 
+            this.btn_findPath.Location = new System.Drawing.Point(89, 180);
+            this.btn_findPath.Name = "btn_findPath";
+            this.btn_findPath.Size = new System.Drawing.Size(75, 23);
+            this.btn_findPath.TabIndex = 6;
+            this.btn_findPath.Text = "Find Path";
+            this.btn_findPath.UseVisualStyleBackColor = true;
+            this.btn_findPath.Click += new System.EventHandler(this.btn_findPath_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "End:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Start:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Point:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Map Size:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Map Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name:";
+            // 
+            // panel_map
+            // 
+            this.panel_map.Controls.Add(this.pic_map);
+            this.panel_map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_map.Location = new System.Drawing.Point(170, 0);
+            this.panel_map.Name = "panel_map";
+            this.panel_map.Size = new System.Drawing.Size(514, 461);
+            this.panel_map.TabIndex = 1;
+            // 
+            // pic_map
+            // 
+            this.pic_map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic_map.Location = new System.Drawing.Point(0, 0);
+            this.pic_map.Name = "pic_map";
+            this.pic_map.Size = new System.Drawing.Size(514, 461);
+            this.pic_map.TabIndex = 0;
+            this.pic_map.TabStop = false;
+            this.pic_map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pic_map_MouseClick);
+            this.pic_map.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_map_MouseMove);
+            // 
+            // lbl_info
+            // 
+            this.lbl_info.AutoSize = true;
+            this.lbl_info.Location = new System.Drawing.Point(7, 155);
+            this.lbl_info.Name = "lbl_info";
+            this.lbl_info.Size = new System.Drawing.Size(41, 12);
+            this.lbl_info.TabIndex = 13;
+            this.lbl_info.Text = "提示：";
+            // 
+            // btn_brower
+            // 
+            this.btn_brower.Location = new System.Drawing.Point(6, 180);
+            this.btn_brower.Name = "btn_brower";
+            this.btn_brower.Size = new System.Drawing.Size(77, 23);
+            this.btn_brower.TabIndex = 14;
+            this.btn_brower.Text = "Select map";
+            this.btn_brower.UseVisualStyleBackColor = true;
+            this.btn_brower.Click += new System.EventHandler(this.btn_brower_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.panel_map);
+            this.Controls.Add(this.gb_mapinfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gb_mapinfo.ResumeLayout(false);
+            this.gb_mapinfo.PerformLayout();
+            this.panel_map.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_map)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_mapinfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel_map;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pic_map;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_findPath;
+        private System.Windows.Forms.Label lbl_end;
+        private System.Windows.Forms.Label lbl_start;
+        private System.Windows.Forms.Label lbl_point;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_info;
+        private System.Windows.Forms.Button btn_brower;
+    }
+}
+
---
+++ Jm.Core.MIr2Test/Form1.cs
@@ -0,0 +1,210 @@
+﻿using Jm.Core.AStartTest;
+using Jm.Core.Mir2.Server.VisualMapInfo.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jm.Core.MIr2Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            width = Width;
+            height = Height;
+        }
+
+        int width, height;
+        ReadMap readMap = new ReadMap();
+        List<Point> points = new List<Point>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_findPath_Click(object sender, EventArgs e)
+        {
+            if(points.Count!=2)
+            {
+                MessageBox.Show("请选择开始、结束点位置!");
+                return;
+            }
+            var start = points[0];
+            var end = points[1];
+
+            AStar aStar = new AStar(readMap.Maze);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tempPoints = aStar.FindPath(new APoint(start.X, start.Y), new APoint(end.X, end.Y));
+            stopwatch.Stop();
+            if (tempPoints==null || tempPoints.Count==0)
+            {
+                lbl_info.Text = "未找到路线！";
+                return;
+            }
+            lbl_info.Text = $"耗时:{stopwatch.ElapsedMilliseconds}\r\n消耗:{tempPoints.Count}";
+
+            Bitmap newBitmap = new Bitmap(readMap.clippingZone.Width, readMap.clippingZone.Height);
+            if (CopyBitmap(readMap.clippingZone, newBitmap))
+            {
+                pic_map.Image = newBitmap;
+            }
+            foreach (var item in tempPoints)
+            {
+                newBitmap.SetPixel(item.X, item.Y, Color.Red);
+            }
+            pic_map.Image = newBitmap;
+        }
+
+        private void pic_map_MouseMove(object sender, MouseEventArgs e)
+        {
+            lbl_point.Text = $"X:{e.X} Y:{e.Y}";
+        }
+        /// <summary>
+        /// 重绘地图
+        /// </summary>
+        private Bitmap reCopyMap()
+        {
+            Bitmap newBitmap = new Bitmap(readMap.Width, readMap.Height);
+            CopyBitmap(readMap.clippingZone, newBitmap);
+            return newBitmap;
+        }
+        private void pic_map_MouseClick(object sender, MouseEventArgs e)
+        {
+            lbl_info.Text = "";
+
+            if(readMap.Maze[e.X,e.Y]==1)
+            {
+                MessageBox.Show("该位置为障碍物!");
+                return;
+            }
+            if (points.Count == 2)
+            {
+                lbl_start.Text = lbl_end.Text = "--";
+                points.Clear();
+                pic_map.Image = reCopyMap();
+            }
+
+            points.Add(new Point(e.X, e.Y));
+
+            var start = points[0];
+            lbl_start.Text = $"X:{start.X} Y:{start.Y}";
+
+            if (points.Count == 2)
+            {
+                var end = points[1];
+                lbl_end.Text = $"X:{end.X} Y:{end.Y}";
+            }
+
+            var bitmap = reCopyMap();
+            foreach (var item in points)
+            {
+                bitmap.SetPixel(item.X, item.Y, Color.Red);
+            }
+            pic_map.Image = bitmap;
+
+        }
+
+        private void btn_brower_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(map)|*.map"; //设置要选择的文件的类型
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;//返回文件的完整路径     
+
+                try
+                {
+                    readMap.mapFile = file;
+                    readMap.Load();
+
+
+                    pic_map.Width = readMap.Width;
+                    panel_map.Width = readMap.Width;
+                    pic_map.Height = readMap.Height;
+
+                    pic_map.Image = reCopyMap();
+
+                    var w = gb_mapinfo.Width + readMap.Width;
+                    if (w < width)
+                        w = width;
+                    this.Width = w;
+                    this.Height = readMap.Height < height ? height : readMap.Height;
+
+                    //this.Width = 999;
+                    //this.Height = 999;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        public bool CopyBitmap(Bitmap source, Bitmap destination)
+        {
+            if ((source.Width != destination.Width) || (source.Height != destination.Height) || (source.PixelFormat != destination.PixelFormat))
+            {
+                return false;
+            }
+
+            int bitdepth_per_pixel = Bitmap.GetPixelFormatSize(source.PixelFormat) / 8;
+
+            if (bitdepth_per_pixel != 1 && bitdepth_per_pixel != 3 && bitdepth_per_pixel != 4)
+            {
+                return false;
+            }
+
+            BitmapData source_bitmapdata = null;
+            BitmapData destination_bitmapdata = null;
+
+            try
+            {
+                source_bitmapdata = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadWrite,
+                                                source.PixelFormat);
+                destination_bitmapdata = destination.LockBits(new Rectangle(0, 0, destination.Width, destination.Height), ImageLockMode.ReadWrite,
+                                                destination.PixelFormat);
+
+                int source_bitmapdata_bitdepth_width = source_bitmapdata.Width * bitdepth_per_pixel;
+                int source_bitmapdata_height = source_bitmapdata.Height;
+                int source_bitmapdata_bitdepth_stride = source_bitmapdata.Stride;
+
+                unsafe
+                {
+                    byte* source_ptr = (byte*)source_bitmapdata.Scan0;
+                    byte* destination_ptr = (byte*)destination_bitmapdata.Scan0;
+
+                    int offset = source_bitmapdata_bitdepth_stride - source_bitmapdata_bitdepth_width;
+
+                    for (int i = 0; i < source_bitmapdata_height; i++)
+                    {
+                        for (int j = 0; j < source_bitmapdata_bitdepth_width; j++, source_ptr++, destination_ptr++)
+                        {
+                            *destination_ptr = *source_ptr;
+                        }
+
+                        source_ptr += offset;
+                        destination_ptr += offset;
+                    }
+                }
+
+                source.UnlockBits(source_bitmapdata);
+                destination.UnlockBits(destination_bitmapdata);
+
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
